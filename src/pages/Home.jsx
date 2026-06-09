68aa3d8bca8ae1869730cc26a59d8edd8567a4c9
gaparker716 <graceparkermt@gmail.com>
2026-06-08 22:04:13 -0700

    fixed button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Icon } from "@iconify/react"
+import resume from "../images/Grace-Parker-Resume.pdf"
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <h1>Grace Parker</h1>
         <p className="lede">A full-stack computer scientist based in Seattle, Washington.</p>
         <div className="cta-row">
-          <a href="src/images/Grace-Parker-Resume.pdf" target="_blank" rel="noreferrer" className="btn btn-primary">
+          <a href={resume} target="_blank" rel="noreferrer" className="btn btn-primary">
             <Icon icon="mdi:file-document-outline" height={24} />Open Resume
           </a>
           <a href="#projects" className="btn btn-ghost">
